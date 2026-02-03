b38d191ff77822c4975ad77ba6a4e7efb0784ba1
solfKwolf <13536812252@163.com>
2026-02-03 22:25:22 +0800

    chore: 迁移到 vite-plugin-pwa 实现 PWA 功能

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
 import { execSync } from 'child_process';
 
 // 获取 git 提交信息
@@ -15,7 +16,84 @@ try {
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'icons/*.svg', 'icons/*.png'],
+      manifest: {
+        name: '在线便签',
+        short_name: '便签',
+        description: '免费、便捷的在线笔记工具，支持快速创建、编辑和管理便签',
+        theme_color: '#fbc02d',
+        background_color: '#ffffff',
+        display: 'standalone',
+        orientation: 'portrait-primary',
+        icons: [
+          {
+            src: 'icons/manifest-icon-192.maskable.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: 'icons/manifest-icon-512.maskable.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: 'favicon.svg',
+            sizes: '32x32',
+            type: 'image/svg+xml',
+            purpose: 'any'
+          },
+          {
+            src: 'icons/apple-icon-180.png',
+            sizes: '180x180',
+            type: 'image/png',
+            purpose: 'any'
+          }
+        ],
+        shortcuts: [
+          {
+            name: '新建便签',
+            short_name: '新建',
+            description: '创建新的便签',
+            url: '?action=new',
+            icons: [
+              {
+                src: 'favicon.svg',
+                sizes: '192x192'
+              }
+            ]
+          }
+        ],
+        categories: ['productivity', 'utilities', 'notes'],
+        lang: 'zh-CN',
+        dir: 'ltr'
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/fonts\.googleapis\.com\//i,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'google-fonts-cache',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365 // 1 year
+              },
+              cacheableResponse: {
+                statuses: [0, 200]
+              }
+            }
+          }
+        ]
+      }
+    })
+  ],
   server: {
     host: '0.0.0.0',
   },
